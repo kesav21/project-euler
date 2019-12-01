1cf777bcc82ef5ce6dcfcb21fff693aef44ad9cc
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2019-11-30 21:52:35 -0700

    Exported sieve to common file

---
+++ tools.py
@@ -0,0 +1,14 @@
+def sieve(n):
+    """
+    an implementation of the sieve of erastothenes,
+    used to find prime numbers
+    """
+    primes = list(range(n + 1))
+    primes[1] = 0
+    for i in primes:
+        if i != 0 and i < int(n ** 0.5):
+            for j in range(2 * i, n + 1, i):
+                primes[j] = 0
+    for i in primes:
+        if i != 0:
+            yield i
