9b293d68c0644a10d19958995a6799e1252876a1
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2019-12-22 20:02:05 -0700

    Added solution to problem 39 (it's in haskell)

---
+++ P039.hs
@@ -0,0 +1,21 @@
+#!/usr/bin/runghc
+
+import           Data.List (elemIndex)
+import           Tools     (factors)
+
+checkA :: Int -> Int -> Bool
+checkA p a = (a * a) `mod` (p - a) == 0
+
+possibleA :: Int -> Int
+possibleA p = length . (filter (checkA p)) $ [1.. k]
+    where k = p `div` 3 - 1
+
+maxSols :: [Int] -> Maybe Int
+maxSols l = (maximum sols) `elemIndex` sols
+    where sols = map (possibleA) l
+
+main :: IO ()
+main = do
+
+    print . maxSols $ [0..1000]
+
