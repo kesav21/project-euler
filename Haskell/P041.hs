781cfcc7971120423479e91c998ab05c8eb9b6a8
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2019-12-22 21:34:08 -0700

    Applied optimization to solution 41

--- Haskell/P041.hs
+++ Haskell/P041.hs
@@ -6,8 +6,19 @@ import           Data.List (permutations)
 nDigitPans :: Int -> [Int]
 nDigitPans n = (map (read::String->Int)) . permutations . concat . (map (show)) $ [1..n]
 
+--
+-- thanks to gamma in the thread:
+-- 2,3,5,6,8,9 digit pandigitals do not need to be checked
+-- 1+2=3 => always divisible by 3
+-- 1+2+3=6 => always divisible by 3
+-- 1+2+3+4+5=15 => always divisible by 3
+-- 1+2+3+4+5+6=21 => always divisible by 3
+-- 1+2+3+4+5+6+7+8=36 => always divisible by 3
+-- 1+2+3+4+5+6+7+8+9=45 => always divisible by 3
+-- that leaves 4 and 7
+--
 allPans :: [Int]
-allPans = concat . (map (nDigitPans)) $ [1..9]
+allPans = concat . (map (nDigitPans)) $ [4,7]
 
 isqrt :: Int -> Int
 isqrt = floor . sqrt . fromIntegral
